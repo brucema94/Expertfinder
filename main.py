00ec4dada1fd8e27143cdf9f70ad7f365dc4a88b
bruce <brucema1994@gmail.com>
2021-06-05 14:19:36 +0200

    lol

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-# os.environ["CHROMEDRIVER"] = r"C:\Users\bruce\Downloads\chromedriver_win32"
 import sys
 sys.path.append(r"C:\Users\bruce\PycharmProjects\ExpertFinder\chromedriver.exe")
 
@@ -7,22 +5,17 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path=r'C:\Users\bruce\PycharmProjects\ExpertFinder\chromedriver.exe')
 
 from linkedin_scraper import Person, actions
-from selenium import webdriver
-#driver = webdriver.Chrome()
 
+#PWD
+from yaml import load as load_yaml
+def config_loader(path):
+    with open(path) as f:
+        raw_dict = load_yaml(f)
+    return  {key:raw_dict[key]['value'] for key in raw_dict.keys()}
+config = config_loader("PassWD.yaml")
+
+# personal login
 email = "brucema1994@gmail.com"
-password = "mgqst59M"
+password = config["PWD"]
 actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
 person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)
-
-
-
-
-#from linkedin_scraper import Person, actions
-#from selenium import webdriver
-#driver = webdriver.Chrome()
-
-#email = "some-email@email.address"
-#password = "password123"
-#actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
-#person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)
